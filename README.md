407f81431911e1867e05e4cda7a5389c9740f44f
AlexanderJollet <alexander.jollet3@gmail.com>
2021-05-28 15:35:18 +0200

    add test file + modif readme file

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 * Docker 
 ## Git clone URL Project
 ## Open terminal, go in the directory project and run 'docker-compose -d --build'
-## If there is problems with the docker-compose file, just run 'npm install' and 'npm start' in the directory 'projet_crud_with_testing'
+### If there is problems with the docker-compose file, just run 'npm install' and 'npm start' in the directory 'projet_crud_with_testing'
+
+### Running test : npm run test
---
+++ server/test/user.test.js
@@ -0,0 +1,48 @@
+const User = require('../model/model');
+const app = require('../../server');
+const request = require('chai');
+const expect = require('chai').expect;
+
+describe("Test API", () =>{
+    beforeEach(async () =>{
+        await User.deleteMany({});
+    });
+
+    describe("POST /add_user", function(){
+        it("Should create and return a user", async function() {
+            let data = {
+                name: "Alex",
+                lastname: "jollet",
+                email: "hi@gmail.com",
+                phone_number: 0645454546,
+            }
+
+            let res = await request(app).create("/api/users").send(data);
+
+            expect(res.status).to.equal(201);
+            expect(res.body.user).to.have.property("Alex", data.name);
+            expect(res.body.user).to.have.property("jollet", data.lastname);
+            expect(res.body.user).to.have.property("hi@gmail.com", data.email);
+            expect(res.body.user).to.have.property(0645454546, data.phone_number);
+        })
+        it("Should return error if wrong phone number / email provided", async function() {
+            let data = {
+                name: "alex",
+                lastname: "jo",
+                email: "hi@gmail.com",
+                phone_number: 0645454546,
+            };
+
+            await request(app).post("/api/users").send(data);
+
+            data = {
+                email: "hi@johndoe.com",//wrong email
+                phone_number: 0645454546,
+            }
+            //Send wrong data and expect error 401
+            let res = await request(app).post("/api/users").send(data);
+
+            expect(res.status).to.equal(401);
+        })
+    })
+})
